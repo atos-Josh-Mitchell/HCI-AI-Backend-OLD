8015b486adfa0b9f4739c07b02f9197f6ee95ae7
atos-Josh-Mitchell <joshua.mitchell@atos.net>
2024-04-02 11:47:04 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, send_from_directory
+import time
+
+app = Flask(__name__, static_folder="static", template_folder="static")
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+# Route to serve static files (e.g., JavaScript, CSS, images)
+# test
+@app.route('/assets/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('../backend/static/assets', filename)
+
+@app.route('/api')
+def hello():
+    return jsonify(message="Hello from Flask!")
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+if __name__ == '__main__':
+    app.run(debug=True)
